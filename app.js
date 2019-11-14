0498fc1f1205d5ce90491062ad88624f3de6dfb9
MarcoVillanueva705 <villanuevamarco98@gmail.com>
2019-11-14 16:36:09 -0700

    thursday commit

--- app.js
+++ app.js
@@ -1,44 +1,40 @@
+let outcomeElem = document.querySelector('#outcome')
+
+
+
 function play(playerChoice) {
   let choices = ["rock", "paper", "scissors"];
   let computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
-  if (playerChoice == "rock" && computerChoice == "rock") {
-    console.log("Tie!");
+  if (playerChoice == computerChoice) {
+    // console.log("Tie!");
+    outcomeElem.innerHTML = "Tie!"
   }
   if (playerChoice == "rock") {
     if (computerChoice == "paper") {
-      console.log("Paper covers rock :(")
-    }
-  }
-  if (playerChoice == "rock") {
-    if (computerChoice == "scissors") {
-      console.log("Nice Job!")
-    }
-  }
-  if (playerChoice == "paper" && computerChoice == "paper") {
-    console.log("Another Tie!");
-  }
-  if (playerChoice == "paper") {
-    if (computerChoice == "rock") {
-      console.log("You Win!")
+      // console.log("Paper covers rock")
+      outcomeElem.innerHTML = "Paper covers rock, you lose!"
+    } else if (computerChoice == "scissors") {
+      // console.log("Nice Job!")
+      outcomeElem.innerHTML = "Nice Job!"
     }
   }
   if (playerChoice == "paper") {
-    if (computerChoice == "scissors") {
-      console.log("Ouch!")
+    if (computerChoice == "rock") {
+      // console.log("You Win!")
+      outcomeElem.innerHTML = "You Win!"
+    } else if (computerChoice == "scissors") {
+      // console.log("Ouch!")
+      outcomeElem.innerHTML = "Ouch!"
     }
-  }
-  if (playerChoice == "scissors" && computerChoice == "scissors") {
-    console.log("Wow! Tied again!");
   }
   if (playerChoice == "scissors") {
     if (computerChoice == "rock") {
-      console.log("Your scissors! Smashed!")
-    }
-  }
-  if (playerChoice == "scissors") {
-    if (computerChoice == "paper") {
-      console.log("Take that, paper!")
+      // console.log("Your scissors! Smashed!")
+      outcomeElem.innerHTML = "Your scissors! Smashed!"
+    } else if (computerChoice == "paper") {
+      // console.log("Take that, paper!")
+      outcomeElem.innerHTML = "Take that, paper!"
     }
   }
 }
